05940f2ac6afc022159f676f5833bc8584f276e3
Oli <oli.de.psycho.gamer@gmail.com>
2018-05-04 02:05:54 +0530

    Add jquery and Bootstrap

---
+++ application/views/inc/header_view.php
@@ -0,0 +1,16 @@
+!<!DOCTYPE html>
+<html>
+<head>
+	<title>CodeIgniter App</title>
+	<link rel="stylesheet" type="text/css" href="<?php base_uri() ?>public/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php base_uri() ?>public/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php base_uri() ?>public/css/style.css">
+
+	<script src"<?php base_uri() ?>public/js/jquery.js"></script>
+	<script src"<?php base_uri() ?>public/js/bootstrap.js"></script>
+</head>
+<body>
+
+<header>
+	CI App
+</header>
