10f6e7709cebcb65c32e8759bad47ae06db5bd5e
Oli <oli.de.psycho.gamer@gmail.com>
2018-05-04 02:16:25 +0530

    Create Home Controller
    1. Create Home Controller
    2. Change default controller from welcome controller to home controller in config/routes.php
    3. Load the URL helper by including it in config/autoload.php

--- application/views/inc/header_view.php
+++ application/views/inc/header_view.php
@@ -1,13 +1,13 @@
-!<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>CodeIgniter App</title>
-	<link rel="stylesheet" type="text/css" href="<?php base_uri() ?>public/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php base_uri() ?>public/css/bootstrap-grid.min.css">
-	<link rel="stylesheet" type="text/css" href="<?php base_uri() ?>public/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>public/css/style.css">
 
-	<script src"<?php base_uri() ?>public/js/jquery.js"></script>
-	<script src"<?php base_uri() ?>public/js/bootstrap.js"></script>
+	<script src"<?php echo base_url() ?>public/js/jquery.js"></script>
+	<script src"<?php echo base_url() ?>public/js/bootstrap.js"></script>
 </head>
 <body>
 
