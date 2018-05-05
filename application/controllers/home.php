32a27d42b34411c79087b41d267192c4361aa391
Oli <oli.de.psycho.gamer@gmail.com>
2018-05-05 15:58:54 +0530

    Create SHA256 hashed password in login
    - Create custom Salt constatn in config/constants.php
    - hash the password in login method of user controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -7,5 +7,13 @@ class Home extends CI_Controller {
 		$this->load->view('home/inc/footer_view');
 	}
 
+	// public function test_code() {
+
+	// 	echo hash('sha256', '123456' . SALT);
+
+	// 	//$this->load->library('encrypt');
+	// 	// echo $this->encrypt->encode('My Secrete Passsword');
+	// 	// echo $this->encrypt->decode('brtTzo3rwLCznM3elUAkHBHsWjSMYWoRsdCbW0SJZzowEpry02W8CrXKPlib3JMr+Mbsl5qqk6EiZlQaldpqXA==');
+	// }
 }
 
