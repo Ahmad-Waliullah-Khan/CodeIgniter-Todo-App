e8a570668a0ea6c8599928031279624810f17899
Oli <oli.de.psycho.gamer@gmail.com>
2018-05-06 20:13:58 +0530

    Create Dashboard view
    Create dashboard.js
    Create template.js
    Create event.js
    Create result.js

--- application/views/dashboard/dashboard_view.php
+++ application/views/dashboard/dashboard_view.php
@@ -1,12 +1,35 @@
-
-<h3>Dashboard</h3>
-
 <div class="row">
-
-	<div id="dashboard-side" class="col-md-4">
-		
-	</div>
-	<div id="dashboard-main" class="offset-md-1 col-md-7">
-		
-	</div>
+    
+    <div id="dashboard-side" class="col-md-4">
+        <form id="create_todo" class="form-horizontal" method="post" action="<?=site_url('api/create_todo')?>">
+            <div class="input-append">
+                <input type="text" name="content" placeholder="Create New Todo Item" />
+                <input type="submit" class="btn btn-success" value="Create" />
+            </div>
+        </form>
+        
+        <div id="list_todo">
+            <span class="ajax-loader-gray"></span>
+        </div>
+    </div>
+    
+    <div id="dashboard-main" class="offset-md-1 col-md-7">
+        <form id="create_note" class="form-horizontal" method="post" action="<?=site_url('api/create_note')?>">
+            <div class="input-append">
+                <input tabindex="1" type="text" name="title" placeholder="Note Title" />
+                <input tabindex="3" type="submit" class="btn btn-success" value="Create" />
+            </div>
+            
+            <div class="clearfix"></div>
+            
+            <textarea tabindex="2" name="content"></textarea>
+            
+        </form>
+        
+        <div id="list_note">
+            <span class="ajax-loader-gray"></span>
+        </div>
+    </div>
+    
+    
 </div>
