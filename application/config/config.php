5a6c8ed9693ba10e02a73624bb2efb5687537956
Oli <oli.de.psycho.gamer@gmail.com>
2018-09-20 15:04:36 +0530

    changed ['base_url'] back to original value in config.php file

--- application/config/config.php
+++ application/config/config.php
@@ -40,13 +40,13 @@ spl_autoload_register(function($class) {
 |
 */
 
-// $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 
-// $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 
-// $config['base_url'] .=str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] .=str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
-$config['base_url'] = 'https://oliz-todo-app-ci.herokuapp.com/';
+// $config['base_url'] = 'https://oliz-todo-app-ci.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
